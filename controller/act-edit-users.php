4781f07224f41a8c2b2a069b371e8ba00357bff4
Fauzan Akbar Almunbaits <fauzanakbaralmunbaits@gmail.com>
2019-01-24 19:17:10 +0700

    create fungsi edit

--- controller/act-edit-users.php
+++ controller/act-edit-users.php
@@ -8,31 +8,40 @@
     $fullname = $_POST['fullname'];
     $otorisasi = $_POST['otorisasi'];
 
-    $photo = $_FILES['photo']['name'];
-    $tmp = $_FILES['photo']['tmp_name'];
-
-    // Rename nama fotonya dengan menambahkan tanggal dan jam upload
-    $photobaru = date('dmYHis').$photo;
-    // Set path folder tempat menyimpan fotonya
-    $path = "../assets/images/".$photobaru;
-
-    if(move_uploaded_file($tmp, $path)){ // Cek apakah gambar berhasil diupload atau tidak
-
-        // Query untuk menampilkan data users berdasarkan id yang dikirim
-        $r = $con->query("SELECT * FROM tb_users WHERE id = '$id_lama'");
-	    foreach ($r as $rr) {
-        // Cek apakah file foto sebelumnya ada di folder images
-        if(is_file("assets/images/".$rr['photo'])) // Jika foto ada
-        unlink("assets/images/".$rr['photo']); // Hapus file foto sebelumnya yang ada di folder images
-        }
-
-        $con->query("UPDATE tb_users SET email='$email', `password`='$password', photo='$photobaru', fullname='$fullname', otorisasi='$otorisasi' WHERE id='$id_lama' ");
+    $con->query("UPDATE tb_users SET email='$email', `password`='$password', fullname='$fullname', otorisasi='$otorisasi' WHERE id='$id_lama' ");
 
         if ($con->affected_rows > 0){
             echo "<script>alert('User telah berhasil diedit');window.location='../index.php?pages=users'</script>";
         }else{
             echo "<script>alert('User telah gagal diedit');window.location='../index.php?pages=users'</script>";
         }
+
+    // $photo = $_FILES['photo']['name'];
+    // $tmp = $_FILES['photo']['tmp_name'];
+
+    // Rename nama fotonya dengan menambahkan tanggal dan jam upload
+    // $photobaru = date('dmYHis').$photo;
+
+    // Set path folder tempat menyimpan fotonya
+    // $path = "../assets/images/".$photobaru;
+
+    // if(move_uploaded_file($tmp, $path)){ // Cek apakah gambar berhasil diupload atau tidak
+
+        // Query untuk menampilkan data users berdasarkan id yang dikirim
+        // $r = $con->query("SELECT * FROM tb_users WHERE id = '$id_lama'");
+	    // foreach ($r as $rr) {
+        // Cek apakah file foto sebelumnya ada di folder images
+        // if(is_file("assets/images/".$rr['photo'])) // Jika foto ada
+        // unlink("assets/images/".$rr['photo']); // Hapus file foto sebelumnya yang ada di folder images
+        // }
+
+        // $con->query("UPDATE tb_users SET email='$email', `password`='$password', photo='$photobaru', fullname='$fullname', otorisasi='$otorisasi' WHERE id='$id_lama' ");
+
+        // if ($con->affected_rows > 0){
+            // echo "<script>alert('User telah berhasil diedit');window.location='../index.php?pages=users'</script>";
+        // }else{
+            // echo "<script>alert('User telah gagal diedit');window.location='../index.php?pages=users'</script>";
+        // }
         
-    }
+    // }
 ?>
--- pages/edit-users.php
+++ pages/edit-users.php
@@ -1,3 +1,11 @@
+<?php
+    $id = $_GET['id'];
+
+    $r = $con->query("SELECT * FROM tb_users WHERE id = '$id'");
+    foreach ($r as $rr) {
+        // $id = $rr['id'];
+?>
+
 <div class="row">
     <div class="col-sm-12">
         <div class="white-box">
@@ -5,40 +13,40 @@
             <p class="text-muted m-b-30 font-13"> Edit Otorisasi Back Office Educode </p>
             <div class="row">
                 <div class="col-sm-12 col-xs-12">
-                    <form action="" method="POST">
+                    <form action="controller/act-edit-users.php?id=<?php echo $id; ?>" method="POST">
                         <div class="form-group">
                             <label for="exampleInputuname">Full Name</label>
                             <div class="input-group">
                                 <div class="input-group-addon"><i class="ti-user"></i></div>
-                                <input type="hidden" name="id" class="form-control" id="exampleInputuname" value="" required>
-                                <input type="text" name="fullname" class="form-control" id="exampleInputuname" value="" required> </div>
+                                <input type="hidden" name="id" class="form-control" id="exampleInputuname" value="<?php echo $rr['id'];?>" required>
+                                <input type="text" name="fullname" class="form-control" id="exampleInputuname" value="<?php echo $rr['fullname'];?>" required> </div>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputEmail1">Email address</label>
                             <div class="input-group">
                                 <div class="input-group-addon"><i class="ti-email"></i></div>
-                                <input type="email" name="email" class="form-control" id="exampleInputEmail1" value="" required> </div>
+                                <input type="email" name="email" class="form-control" id="exampleInputEmail1" value="<?php echo $rr['email'];?>" required> </div>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputpwd1">Password</label>
                             <div class="input-group">
                                 <div class="input-group-addon"><i class="ti-lock"></i></div>
-                                <input type="password" name="password" class="form-control" id="exampleInputpwd1" value="" required> </div>
+                                <input type="password" name="password" class="form-control" id="exampleInputpwd1" required> </div>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputpwd2">Otorisasi</label>
                             <div class="input-group">
                                 <div class="input-group-addon"><i class="fa fa-users"></i></div>
                                 <select class="form-control" name="otorisasi" required>
-                                    <option>Administrator</option>
-                                    <option>User</option>
+                                    <option <?php if($rr['otorisasi']=='Administrator'){echo "selected"; } ?> >Administrator</option>
+                                    <option <?php if($rr['otorisasi']=='User'){echo "selected"; } ?> >User</option>
                                 </select>
                         </div>
                         <div class="form-group">
                             
                         </div>
                         <div class="form-group text-right">
-                            <button class="btn btn-inverse waves-effect waves-light  m-r-10"><a href="#" style="color:white;">Cancel</a></button>
+                            <button class="btn btn-inverse waves-effect waves-light  m-r-10"><a href="index.php?pages=users" style="color:white;">Cancel</a></button>
 	                        <button type="submit" class="btn btn-success waves-effect waves-light">Submit</button>
 	                    </div>
                     </form>
@@ -47,3 +55,7 @@
         </div>
     </div>
 </div>
+
+<?php
+}
+?>
