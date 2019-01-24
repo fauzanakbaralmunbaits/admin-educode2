f3d81d58f8c12ef556a1d4f6c3e3eb4a851d7e11
Fauzan Akbar Almunbaits <fauzanakbaralmunbaits@gmail.com>
2019-01-24 19:16:13 +0700

    edit all

--- pages/503.php
+++ pages/503.php
@@ -38,7 +38,7 @@
                 <h1>503</h1>
                 <h3 class="text-uppercase">This site is getting a up in few minute.</h3>
                 <p class="text-muted m-t-30 m-b-30">Please try after some time</p>
-                <a href="index.html" class="btn btn-info btn-rounded waves-effect waves-light m-b-40">Back to home</a> </div>
+                <a href="index.php?pages=home" class="btn btn-info btn-rounded waves-effect waves-light m-b-40">Back to home</a> </div>
             <footer class="footer text-center">2017 © Cubic Admin.</footer>
         </div>
     </section>
--- pages/users.php
+++ pages/users.php
@@ -1,8 +1,8 @@
 <div class="row">
     <div class="col-sm-12">
         <div class="white-box">
-            <h3 class="box-title m-b-0">Data Table</h3>
-            <p class="text-muted m-b-30">Data table example</p>
+            <h3 class="box-title m-b-0">Data Users <button style="float: right;" class="btn btn-success waves-effect waves-light"><a href="index.php?pages=add-users" style="color: white;">Add User</a></button></h3>
+            <p class="text-muted m-b-30">Data User Educode Back Office</p>
             <div class="table-responsive">
                 <table id="myTable" class="table table-striped">
                     <thead>
@@ -15,20 +15,24 @@
                         </tr>
                     </thead>
                     <tbody>
+
+                        <?php
+                            $r = $con->query("SELECT * FROM tb_users");
+                            while ($rr = $r->fetch_array()) {
+                        ?>
+
                         <tr>
-                            <td><center>1</center></td>
-                            <td>Tiger Nixon</td>
-                            <td>System Architect</td>
-                            <td>Edinburgh</td>
-                            <th><a href=""><i class="fa fa-pencil"></i></a> | <a href="" onclick="return confirm('Yakin ingin hapus data?')"><i class="fa fa-trash"></i></a></th>
+                            <td><center>-</center></td>
+                            <td><?php echo $rr['fullname'];?></td>
+                            <td><?php echo $rr['email'];?></td>
+                            <td><?php echo $rr['otorisasi'];?></td>
+                            <th><a href="index.php?pages=edit-users&id=<?php echo $rr['id'];?>"><i class="fa fa-pencil"></i></a> | <a href="controller/act-delete-users.php?id=<?php echo $rr['id'];?>" onclick="return confirm('Yakin ingin hapus data?')"><i class="fa fa-trash"></i></a></th>
                         </tr>
-                        <tr>
-                            <td><center>2</center></td>
-                            <td>Tiger Nixon</td>
-                            <td>System Architect</td>
-                            <td>Edinburgh</td>
-                            <th><a href=""><i class="fa fa-pencil"></i></a> | <a href="" onclick="return confirm('Yakin ingin hapus data?')"><i class="fa fa-trash"></i></a></th>
-                        </tr>
+
+                        <?php
+                            }
+                        ?>
+
                     </tbody>
                 </table>
             </div>
