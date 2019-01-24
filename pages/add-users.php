851973ae5777d2414f3f0217c5951b9ab196cdbb
Fauzan Akbar Almunbaits <fauzanakbaralmunbaits@gmail.com>
2019-01-24 17:36:04 +0700

    edit page add users

--- pages/add-users.php
+++ pages/add-users.php
@@ -5,30 +5,30 @@
             <p class="text-muted m-b-30 font-13"> Tambah Otorisasi Back Office Educode </p>
             <div class="row">
                 <div class="col-sm-12 col-xs-12">
-                    <form>
+                    <form action="" method="POST">
                         <div class="form-group">
                             <label for="exampleInputuname">Full Name</label>
                             <div class="input-group">
                                 <div class="input-group-addon"><i class="ti-user"></i></div>
-                                <input type="text" class="form-control" id="exampleInputuname" required> </div>
+                                <input type="text" name="fullname" class="form-control" id="exampleInputuname" required> </div>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputEmail1">Email address</label>
                             <div class="input-group">
                                 <div class="input-group-addon"><i class="ti-email"></i></div>
-                                <input type="email" class="form-control" id="exampleInputEmail1" required> </div>
+                                <input type="email" name="email" class="form-control" id="exampleInputEmail1" required> </div>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputpwd1">Password</label>
                             <div class="input-group">
                                 <div class="input-group-addon"><i class="ti-lock"></i></div>
-                                <input type="password" class="form-control" id="exampleInputpwd1" required> </div>
+                                <input type="password" name="password" class="form-control" id="exampleInputpwd1" required> </div>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputpwd2">Otorisasi</label>
                             <div class="input-group">
                                 <div class="input-group-addon"><i class="fa fa-users"></i></div>
-                                <select class="form-control" required>
+                                <select class="form-control" name="otorisasi" required>
                                     <option>Administrator</option>
                                     <option>User</option>
                                 </select>
