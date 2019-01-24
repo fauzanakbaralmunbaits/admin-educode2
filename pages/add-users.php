073a310c35959cf5f1ae5ac0a4b90e944a4df241
Fauzan Akbar Almunbaits <fauzanakbaralmunbaits@gmail.com>
2019-01-24 17:29:15 +0700

    edit page add user

--- pages/add-users.php
+++ pages/add-users.php
@@ -1,6 +1,48 @@
 <div class="row">
     <div class="col-sm-12">
         <div class="white-box">
+        	<h3 class="box-title m-b-0">Add User</h3>
+            <p class="text-muted m-b-30 font-13"> Tambah Otorisasi Back Office Educode </p>
+            <div class="row">
+                <div class="col-sm-12 col-xs-12">
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputuname">Full Name</label>
+                            <div class="input-group">
+                                <div class="input-group-addon"><i class="ti-user"></i></div>
+                                <input type="text" class="form-control" id="exampleInputuname" required> </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <div class="input-group">
+                                <div class="input-group-addon"><i class="ti-email"></i></div>
+                                <input type="email" class="form-control" id="exampleInputEmail1" required> </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputpwd1">Password</label>
+                            <div class="input-group">
+                                <div class="input-group-addon"><i class="ti-lock"></i></div>
+                                <input type="password" class="form-control" id="exampleInputpwd1" required> </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputpwd2">Otorisasi</label>
+                            <div class="input-group">
+                                <div class="input-group-addon"><i class="fa fa-users"></i></div>
+                                <select class="form-control" required>
+                                    <option>Administrator</option>
+                                    <option>User</option>
+                                </select>
+                        </div>
+                        <div class="form-group">
+                            
+                        </div>
+                        <div class="form-group text-right">
+                            <button class="btn btn-inverse waves-effect waves-light  m-r-10"><a href="#" style="color:white;">Cancel</a></button>
+	                        <button type="submit" class="btn btn-success waves-effect waves-light">Submit</button>
+	                    </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </div>
